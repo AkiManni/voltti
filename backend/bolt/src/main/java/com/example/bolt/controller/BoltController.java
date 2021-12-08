70b9e98aafda671a8699507c330d42fc6e922fc8
Andy <pmduck@hotmail.com>
2021-12-08 12:03:29 +0200

    crossorigin 2

--- backend/bolt/src/main/java/com/example/bolt/controller/BoltController.java
+++ backend/bolt/src/main/java/com/example/bolt/controller/BoltController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bolt")
-@CrossOrigin(origins = "localhost:3000")
+@CrossOrigin(origins = "localhost:3000/")
 public class BoltController {
     @Autowired
     private UserRepository us;
